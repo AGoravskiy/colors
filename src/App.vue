e12b822cb30422a2b87b2d3e59ce79493ed6047c
aleksey <narchuk.aleksey@gmail.com>
2021-08-22 23:43:09 +0300

    feat: add electron and ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,62 @@
-<template>
-  <router-view />
-</template>
 <script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'App'
-})
+import DateForm from './components/date-form.vue'
+export default {
+  name: 'App',
+  components: {
+    DateForm
+  },
+  data() {
+    return {
+      interval: null,
+      time: null,
+      date: null,
+      dayOfWeek: null,
+      model: null
+    }
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  created() {
+    this.interval = setInterval(() => {
+      this.time = Intl.DateTimeFormat(navigator.language, {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      }).format();
+      this.dayOfWeek = Intl.DateTimeFormat(navigator.language, {
+        weekday: 'long'
+      }).format().toUpperCase();
+      this.date = Intl.DateTimeFormat(navigator.language, {
+        day: 'numeric',
+        month: 'numeric',
+        year:'numeric'
+      }).format();
+    }, 1000)
+  }
+}
 </script>
+
+<template>
+  <div id="q-app" class="column content-center justify-center relative-position">
+    <q-card class="my-card column content-center">
+      <q-card-section class="text-center">
+        {{time}}
+      </q-card-section>
+      <q-card-section class="text-center">
+        {{dayOfWeek}}
+      </q-card-section>
+      <q-card-section class="text-center">
+        {{date}}
+      </q-card-section>
+    </q-card>
+    <q-btn flat round color="primary" icon="date_range" class="absolute-top-right q-ma-md">
+      <q-menu
+        anchor="top left"
+        self="top right"
+      >
+        <date-form></date-form>
+      </q-menu>
+    </q-btn>
+  </div>
+</template>
---
+++ src/components/date-form.vue
@@ -0,0 +1,47 @@
+<script>
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+
+export default {
+  name: 'date-form',
+  setup () {
+    const $q = useQuasar()
+    // const filePath = path.join(app.getPath('userData'), '/some.file')
+
+    const datesModel = ref(null)
+
+    return {
+      datesModel,
+      events: [],
+
+      onSubmit () {
+        console.log(datesModel)
+        datesModel.value = null
+        //console.log(app.getPath('filePath'));
+      },
+
+      onReset () {
+        datesModel.value = null
+        console.log(datesModel)
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="q-pa-md" style="max-width: 400px">
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-date name="wedding" v-model="datesModel" range multiple/>
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </div>
+</template>
